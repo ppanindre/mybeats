8dff4f0acab89b7450e6e6ce61a5352e53759703
PraneethKumarT <praneeth.kumt@gmail.com>
2024-04-30 11:43:31 -0400

    Circle ci fails

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to equal 3', () => {
     expect(1 + 3).toBe(4);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 3).toBe(5);
+});
