b6af02f500e0335e5d81631db648daf66fd04177
MetunNivin <metunnivin@gmail.com>
2024-08-03 00:22:18 -0400

    Refactored Consult doctors page

---
+++ constants/sliderCardData.jsx
@@ -0,0 +1,21 @@
+export const sliderCardData = [
+    {
+        id: "1",
+        title: "20% OFF on first video consultation.",
+        description: "Get a special discount on your first appointment!",
+        buttonText: "See Doctor Now",
+    },
+    {
+        id: "2",
+        title: "Free Follow-up Consultation",
+        description:
+            "Book your initial consultation and get a follow-up for free!",
+        buttonText: "Book Appointment",
+    },
+    {
+        id: "3",
+        title: "Family Health Plan",
+        description: "Secure your family with our tailored health plans.",
+        buttonText: "Learn More",
+    },
+];
