2dd26d814dec8eab73d58af4f09aa72377fd71ea
imtanmay21 <imtanmay2112@gmail.com>
2024-08-18 21:12:16 -0400

    (fix) -> Changed the camera to expo-camera for upload prescription

--- src/app/screens/mybeatsScreens/PrescriptionCamera.jsx
+++ src/app/screens/mybeatsScreens/PrescriptionCamera.jsx
@@ -1,7 +1,7 @@
 // import React, { useRef, useEffect, useState } from 'react';
 // import { View, TouchableOpacity, PermissionsAndroid, Platform, Text } from 'react-native';
 // import { RNCamera } from 'react-native-camera';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 
 // const CameraView = ({ onCapture, onCancel }) => {
 //   const cameraRef = useRef(null);
@@ -22,7 +22,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 //         );
 //         setIsCameraAuthorized(granted === PermissionsAndroid.RESULTS.GRANTED);
 //       } else {
-//         setIsCameraAuthorized(true); 
+//         setIsCameraAuthorized(true);
 //       }
 //     }
 
@@ -66,15 +66,55 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 // export default CameraView;
 
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import AppButton from "../../components/Buttons/AppButton";
 
-const CameraView = () => {
+const PrescriptionCamera = ({ onCapture, onCancel }) => {
+  const cameraRef = useRef();
+
+  const [permission, requestPermission] = useCameraPermissions();
+
+  useEffect(() => {
+    console.log("permisison", permission);
+
+    if (!permission) {
+      console.log("requesting permission");
+      requestPermission();
+    }
+  }, []);
+
+  const captureImage = () => {
+    cameraRef.current.takePictureAsync({
+      base64: false,
+      onPictureSaved: async (picture) => {
+        try {
+          onCapture(picture.uri);
+        } catch (error) {
+          console.error("Error when taking picture", error);
+        }
+      },
+    });
+  };
+
   return (
-    <View>
-      <Text>CameraView</Text>
+    <View className="h-full">
+      <CameraView className="flex-1" ref={cameraRef} facing="back" />
+      <View className=" flex-row p-5 bg-light space-x-3 absolute bottom-0 w-[100%]">
+        <View className="flex-1">
+          <AppButton
+            btnLabel="Take photo"
+            variant="primary"
+            onPress={captureImage}
+          />
+        </View>
+        <View className="flex-1">
+          <AppButton btnLabel="Cancel" variant="light" onPress={onCancel} />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default CameraView
+export default PrescriptionCamera;
